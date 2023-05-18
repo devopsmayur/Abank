684b0f873935e7c57976280c3a1db3ce325c1b1f
Mayur <99334268+devopsmayur@users.noreply.github.com>
2023-05-18 18:54:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+    tfe = {
+      version = "~> 0.44.0"
     }
   }
+}
 
   required_version = ">= 1.2.0"
 }
